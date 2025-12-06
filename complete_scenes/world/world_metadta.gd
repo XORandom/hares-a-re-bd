1293b77a2aaad1e8e6aeac4287a39355da911220
XORandom <XORandom@users.noreply.github.com>
2025-12-06 16:47:10 +0300

    create world_metadata
    отвечает за контекст действий в сцене

---
+++ complete_scenes/world/world_metadta.gd
@@ -0,0 +1,10 @@
+class_name WorldMetadata
+extends Node
+
+## Данные о мире
+##
+## Должны быть в каждом мире,
+## устанавливают необходимые параметры управления (G.U.I.D.E.),
+
+## Какой режим привязки клавиш использовать
+@export var context_mode: GUIDEMappingContext = preload("uid://cflgfu33ylmjg")
