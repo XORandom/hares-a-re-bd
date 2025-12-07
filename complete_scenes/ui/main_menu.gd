91bf9005767645104fbdeea1138f3877e43e5217
XORandom <XORandom@users.noreply.github.com>
2025-12-08 01:25:17 +0300

    Update main_menu.gd
    Update scene_manager.gd

--- complete_scenes/ui/main_menu.gd
+++ complete_scenes/ui/main_menu.gd
@@ -10,11 +10,15 @@ extends Control
 func _on_start_pressed() -> void:
 	await get_tree().create_timer(1).timeout
 	var tween: Tween = create_tween()
-	tween.tween_property(sub_viewport_container.get_material(), "shader_parameter/movement", 1., 2.0)
-	#SceneManager.call_deferred("change_scene", game_scene_path)
-	await tween.finished
-	SceneManager.change_scene(game_scene_path)
+	tween.tween_property(sub_viewport_container.get_material(), "shader_parameter/movement", 1., 1.0)
+	#HACK это грязный метод. Я сначала начинаю загрузку уровня, но не перехожу
 
+	SceneManager.change_scene_after_tween(game_scene_path)
+	await tween.finished
+	#HACK а после уже перехожу
+	SceneManager.call_deferred("change_scene_instant")
+	# Проблема в том, что я не вижу разницы в их работе
+	#SceneManager.call_deferred("change_scene", game_scene_path)
 
 func _on_settings_pressed() -> void:
 	pass
