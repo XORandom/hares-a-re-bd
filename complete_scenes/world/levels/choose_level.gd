8b99115ec08a0286948af484a694a4e909ac3786
XORandom <XORandom@users.noreply.github.com>
2025-12-07 21:24:05 +0300

    add path to ice world

--- complete_scenes/world/levels/choose_level.gd
+++ complete_scenes/world/levels/choose_level.gd
@@ -1,6 +1,11 @@
 extends Control
 
 
+## Путь к ледяному миру
+@export_file("*.tscn") var ice_level_path : String = "uid://b2ptp60qptf3r"
+@export_file("*.tscn") var water_level_path : String = "uid://b2ptp60qptf3r"
+@export_file("*.tscn") var jungle_level_path : String = "uid://b2ptp60qptf3r"
+
 
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
@@ -15,12 +20,15 @@ func  _ready() -> void:
 
 
 func _on_planet_card_jungle_pressed() -> void:
+	SceneManager.call_deferred("change_scene", jungle_level_path)
 	pass # Replace with function body.
 
 
 func _on_planet_card_water_pressed() -> void:
+	SceneManager.call_deferred("change_scene", water_level_path)
 	pass # Replace with function body.
 
 
 func _on_planet_card_ice_pressed() -> void:
+	SceneManager.call_deferred("change_scene", ice_level_path)
 	pass # Replace with function body.
