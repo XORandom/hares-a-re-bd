acadbd2d43d3cc64efe8d08b5c3345c5f9c687c8
XORandom <XORandom@users.noreply.github.com>
2025-12-09 22:24:33 +0300

    TODO aim modular upgrade
    хочется писать красивый код, так что пусть будет к тому моменту, как дойдут руки
    
    Идея в том, что это отдельная сцена, прикрепляемая к пуле, а потом будет m_aim_bullet_stategy
    который будет ее хранить как упакованную сцену и в функции apply_upgrade этот модуль добавляется к только что созданной пуле

---
+++ complete_scenes/entities/bullet_generator/modular_upgrades/m_strategies/m_aim_bullet_stategy.gd
@@ -0,0 +1,14 @@
+class_name AimBulletStrategy
+extends BaseBulletStrategy
+
+## Модуль изменяющий урон пули
+##
+##
+
+## Добавляемый урон пуле
+@export var aim_module_scene: PackedScene = preload("uid://c3datcrbyww8n")
+
+## Применение обновлений
+func apply_upgrade(bullet: BulletBase)-> void:
+	var aim_module: Node2D = aim_module_scene.instantiate()
+	bullet.add_child(aim_module)
