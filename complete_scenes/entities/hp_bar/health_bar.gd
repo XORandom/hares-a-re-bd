3a4926943f5dc679ba0f6a865abd578892eab727
XORandom <XORandom@users.noreply.github.com>
2025-12-10 16:39:06 +0300

    hp+

--- complete_scenes/entities/hp_bar/health_bar.gd
+++ complete_scenes/entities/hp_bar/health_bar.gd
@@ -11,8 +11,8 @@ extends ProgressBar
 @export var initial_health: float = 100.:
 	get:
 		return initial_health
-	set(value):
-		initial_health = value
+	set(new_value):
+		initial_health = new_value
 
 ## Показанное здоровье
 var health: float = 0. : set = _set_health
@@ -23,21 +23,25 @@ var health: float = 0. : set = _set_health
 ## Таймер, отмеряющий, сколько времени
 ## будут показаны повреждения на полоске здоровья
 @onready var timer: Timer = $Timer
+@onready var audio_stream_player: AudioStreamPlayer = $AudioStreamPlayer
+
+
+@onready var label_anim: LabelAnim = $LabelAnim
 
 
 func _ready() -> void:
 	init_health(initial_health)
+	label_anim.text = str(initial_health)
 
 ## Сеттер переменной здоровья
 func _set_health(new_health: float) -> void:
-	print("вызвано изменение здоровья")
 	## Функция, которая будет вызываться каждый раз, когда мы хотим изменить переменную health
 	var prev_health: float = health
 	# Нельзя установить здоровье выше максимального
 	health = min(max_value, new_health)
 	value = health
+	label_anim.text = str(health)
 	if health <=0.:
-		print("здоровье опустилось до нуля")
 		queue_free()
 
 	if health < prev_health: # Если был нанесен урон
