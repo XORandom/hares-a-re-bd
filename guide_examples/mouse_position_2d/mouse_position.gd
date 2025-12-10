2d7a1d2cfd7013ada318f1d26e2a3eb6ae399b62
XORandom <XORandom@users.noreply.github.com>
2025-12-10 13:35:52 +0300

    Update mouse_position.gd

--- guide_examples/mouse_position_2d/mouse_position.gd
+++ guide_examples/mouse_position_2d/mouse_position.gd
@@ -1,5 +1,5 @@
 ## This example shows how to get access to the mouse cursor without being
-## specific about where the input comes from. 
+## specific about where the input comes from.
 extends Node2D
 
 
@@ -17,10 +17,10 @@ func _ready():
 func _spawn_godot_head():
 	# Gets the mouse cursor from G.U.I.D.E. Note how the Canvas Coordinates
 	# modifier automatically gives us mouse coordinates in canvas space
-	# which means we don't need to take into acount the camera panning and 
+	# which means we don't need to take into acount the camera panning and
 	# zoom level and can just use the coordinates we get to directly place
-	# a Godot head at the cursor position. 
+	# a Godot head at the cursor position.
 	var head = godot_head_scene.instantiate()
 	add_child(head)
-	
+
 	head.global_position = cursor.value_axis_2d
