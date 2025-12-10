0ab2206fcd6db24b516dfbc8dbf9deadd4c975e4
XORandom <XORandom@users.noreply.github.com>
2025-12-10 10:54:54 +0300

    Update base_eninty.gd
    добавил характеристики для изменения скорости
    
    Менять надо только speed, остальное можно настраивать, но так-то это константы, поэтому предупреждения

--- complete_scenes/entities/base_eninty.gd
+++ complete_scenes/entities/base_eninty.gd
@@ -7,10 +7,37 @@ extends CharacterBody2D
 ## Принимает полученный урон и направление отбрасывания
 signal take_damage(damage:float, knockback_direction: Vector2)
 
-## скорость движения
-@export var speed: float = 100.
+## текущая скорость
+@export var speed: float = 300.:
+	get:
+		return speed
+	set(value):
+		speed = value
+## максимальная скорость, к примеру бега
+@export var speed_max: float = 600.:
+	get:
+		return speed_max
+	set(value):
+		push_error("Это значение не надо менять")
+		speed_max = value
+## скорость минимальная скорость перемещения (для подкрадывания)
+@export var speed_min: float = 100.:
+	get:
+		return speed_min
+	set(value):
+		push_error("Это значение не надо менять")
+		speed_min = value
+## нормальная скорость перемещения, например для ходьбы
+@export var speed_normal: float = 300.:
+	get:
+		return speed_normal
+	set(value):
+		push_error("Это значение не надо менять")
+		speed_normal = value
+
 ## То, как объект будет выглядеть на карте
 @export var map_icon: Texture2D
+## Уровень здоровья
 @export var hp: float = 100.0
 
 @abstract
